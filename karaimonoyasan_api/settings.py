0bd02d85b1a86b6efb10925df15d382a5d00489a
t-nagasaka <66255777+t-nagasaka@users.noreply.github.com>
2021-12-27 17:46:24 +0900

    create develop branch

--- karaimonoyasan_api/settings.py
+++ karaimonoyasan_api/settings.py
@@ -5,11 +5,6 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 try:
     from .local_settings import *
 except ImportError:
